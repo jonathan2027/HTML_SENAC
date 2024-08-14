ebda12a503446148b611239631716062262875d3
JONATHAN ANTONY QUEIROZ CORREIA <jonathan.aqcorreia@senacsp.edu.br>
2024-08-14 18:48:55 -0300

    salvar

---
+++ JavaScript/Funcoes/saudacao.js
@@ -0,0 +1,7 @@
+function saudacao(){
+    let nome = prompt('Digite seu nome')
+    console.log('Seja bem vindo!' + nome);
+    document.write('<h1>Seja bem vindo '+ nome + '</h1>');
+}
+
+saudacao();
